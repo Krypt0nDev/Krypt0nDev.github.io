104edb257a408eb9adb435e310796741faf7108c
Krypton <83867041+Krypt0nDev@users.noreply.github.com>
2024-02-11 13:05:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,25 @@
-const container = document.getElementById("container");
-const imageOne = document.querySelector(".image-1");
-const imageTwo = document.querySelector(".image-2");
-const btnYes = document.querySelector(".btn-yes");
-const btnNo = document.querySelector(".btn-no");
+const loveMessages = [
+    "רוצה איתך מאה אלף נקניקים",
+    "אוהב את דני עד השמיים מאוהב לדני בעיניים",
+    "אוהב אותך מאוד",
+    "מה ההבדל בין צ'ס לדני? את דני אני אוהב יותר",
+    "חולה בבית וחולה עלייך",
+    "בא לי לחבק אותך",
+    "רוצה להיות חברה שלי?",
+    "מה ההבדל ביו כל בת ישראל לדני? דני יפה",
+    "תגידי את דני? יופי מעולה",
+    "אם היה לי נקניק על כל טיפת אהבה אלייך לא היו נשארים נקניקים לאמץ :("
 
-function getRandomNumber(min, max) {
-  // Calculate the random number between min and max (inclusive)
-  const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
 
-  return randomNumber;
-}
 
-btnNo.addEventListener("mouseover", (event) => {
-  const containerHeight = container.getBoundingClientRect().height;
-  const containerWidth = container.getBoundingClientRect().width;
-  const btnHeight = btnNo.getBoundingClientRect().height;
-  const btnWidth = btnNo.getBoundingClientRect().width;
-  const btnTop = btnNo.getBoundingClientRect().top;
-  const btnLeft = btnNo.getBoundingClientRect().left;
 
-  let newTop = btnTop;
-  let newLeft = btnLeft;
-  while (Math.abs(newTop - btnTop) < containerHeight / 3) {
-    newTop = getRandomNumber(0, containerHeight - btnHeight);
-  }
 
-  while (Math.abs(newLeft - btnLeft) < containerWidth / 3) {
-    newLeft = getRandomNumber(0, containerWidth - btnWidth);
-  }
+];
 
-  btnNo.style.top = Math.floor(newTop) + "px";
-  btnNo.style.left = Math.floor(newLeft) + "px";
-});
+const loveMessageElement = document.getElementById('love-message');
+const changeMessageButton = document.getElementById('change-message');
 
-btnYes.addEventListener("click", (e) => {
-  btnNo.classList.add("hide");
-  imageOne.classList.add("hide");
-  imageTwo.classList.remove("hide");
+changeMessageButton.addEventListener('click', function() {
+    const randomIndex = Math.floor(Math.random() * loveMessages.length);
+    loveMessageElement.textContent = loveMessages[randomIndex];
 });
